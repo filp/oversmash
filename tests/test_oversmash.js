9a5a41b25aca7c1f31108875fe0219750d716643
Filipe Dobreira <dobreira@gmail.com>
2017-10-24 17:42:35 +0100

    Add basic smoke tests

---
+++ tests/test_oversmash.js
@@ -0,0 +1,38 @@
+// Enable debug logging through the debug package
+process.env['DEBUG'] = 'oversmash'
+
+import assert from 'assert'
+import oversmash from '../lib'
+
+// Test names taken from masteroverwatch top lists at random. If you see yourself
+// and would rather not be here, please open an issue!
+
+async function testPlayerProfile (ow) {
+  const p = await ow.player('HaventMetYou-2451')
+
+  assert.equal(p.name, 'HaventMetYou-2451')
+  assert.equal(p.accounts.length, 3)
+}
+
+async function testPlayerStats (ow) {
+  const p = await ow.playerStats('HaventMetYou-2451', 'us', 'pc')
+
+  assert.equal(p.name, 'HaventMetYou-2451')
+  assert.equal(p.region, 'us')
+  assert.equal(p.platform, 'pc')
+
+  // Make sure we successfully extracted the competitive rank
+  assert(p.stats.competitiveRank > 0)
+}
+
+async function runTests () {
+  const ow = oversmash()
+
+  await testPlayerProfile(ow)
+  await testPlayerStats(ow)
+}
+
+runTests()
+  .then(() => {
+    console.log('OK!')
+  })
