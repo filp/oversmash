820ba42b627758beed44e6257d2ed12447bcbb6c
Filipe Dobreira <dobreira@gmail.com>
2019-09-21 16:47:52 +0100

    Add support for snapshotting and comparison at test time

---
+++ tests/data_snapshot.js
@@ -0,0 +1,16 @@
+import { promisify } from 'util'
+import { writeFile } from 'fs'
+import oversmash from '../lib'
+
+const account = 'FATCOTTON420-2476'
+const writeFileAsync = promisify(writeFile)
+
+async function captureSnapshots (ow) {
+  await writeFileAsync('./tests/snapshots/profile.json', JSON.stringify(await ow.player(account.replace('-', '#'))))
+  await writeFileAsync('./tests/snapshots/stats.json', JSON.stringify(await ow.playerStats(account, 'pc')))
+}
+
+captureSnapshots(oversmash())
+  .then(() => {
+    console.log('captured snapshots!')
+  })
--- tests/test_oversmash.js
+++ tests/test_oversmash.js
@@ -1,14 +1,52 @@
 import assert from 'assert'
 import oversmash from '../lib'
+import { promisify } from 'util'
+import { readFile } from 'fs'
+import objectPath from 'object-path'
+import traverse from 'traverse'
+import debug from 'debug'
 
-async function testPlayerProfile (ow) {
+const log = debug('oversmash:snapshot')
+const readFileAsync = promisify(readFile)
+
+async function loadSnapshot (name) {
+  const data = await readFileAsync(`./tests/snapshots/${name}.json`)
+  return JSON.parse(data)
+}
+
+function compareToSnapshot (snapshot, data) {
+  traverse(data).forEach(function (node) {
+    if (Array.isArray(node) || (typeof node === 'object' && node !== null)) return
+
+    const path = this.path.join('.')
+    const compareTo = objectPath.get(snapshot, path)
+    const t = typeof node
+    const ct = typeof compareTo
+
+    if (t === 'undefined' || node === null) {
+      log(`bad-value(${path}): is ${t === null ? 'null' : 'undefined'}`)
+    }
+
+    if (t !== ct) {
+      log(`type-diverged(${path}): ${t} !== ${ct}`)
+    }
+
+    if (node !== compareTo) {
+      log(`diverged(${path}): ${node} !== ${compareTo}`)
+    }
+  })
+}
+
+async function testPlayerProfile (ow, snapshot) {
   const p = await ow.player('FATCOTTON420#2476')
 
   assert.equal(p.name, 'FATCOTTON420-2476')
   assert.equal(p.accounts.length, 1)
+
+  compareToSnapshot(snapshot, p)
 }
 
-async function testPlayerStats (ow) {
+async function testPlayerStats (ow, snapshot) {
   const p = await ow.playerStats('FATCOTTON420-2476', 'pc')
 
   assert.equal(p.name, 'FATCOTTON420-2476')
@@ -23,13 +61,15 @@ async function testPlayerStats (ow) {
 
   // Make sure diacritics replacement is working as intended:
   assert(p.stats.quickplay.lucio.combat.all_damage_done > 0)
+
+  compareToSnapshot(snapshot, p)
 }
 
 async function runTests () {
   const ow = oversmash()
 
-  await testPlayerProfile(ow)
-  await testPlayerStats(ow)
+  await testPlayerProfile(ow, await loadSnapshot('profile'))
+  await testPlayerStats(ow, await loadSnapshot('stats'))
 }
 
 runTests()
