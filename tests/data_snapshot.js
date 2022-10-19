ae2daad7173cc1d1797229e0bb83eec6e44aa3c2
Filipe Dobreira <dobreira@gmail.com>
2022-10-19 23:19:46 +0100

    update eslint config

--- tests/data_snapshot.js
+++ tests/data_snapshot.js
@@ -1,18 +1,22 @@
-import { promisify } from 'util'
-import { writeFile } from 'fs'
-import oversmash from '../lib'
+import fs from 'fs';
+import oversmash from '../lib';
 
 // Picked top player in overbuff, if this is you and you'd rather not be here,
 // please send me a message!
-const account = 'adara#21451'
-const writeFileAsync = promisify(writeFile)
+const account = 'adara#21451';
 
-async function captureSnapshots (ow) {
-  await writeFileAsync('./tests/snapshots/profile.json', JSON.stringify(await ow.player(account)))
-  await writeFileAsync('./tests/snapshots/stats.json', JSON.stringify(await ow.playerStats(account, 'pc')))
+async function captureSnapshots(ow) {
+  await fs.promises.writeFile(
+    './tests/snapshots/profile.json',
+    JSON.stringify(await ow.player(account))
+  );
+
+  await fs.promises.writeFile(
+    './tests/snapshots/stats.json',
+    JSON.stringify(await ow.playerStats(account, 'pc'))
+  );
 }
 
-captureSnapshots(oversmash({ normalizeNamesAs: 'camel' }))
-  .then(() => {
-    console.log('captured snapshots!')
-  })
+captureSnapshots(oversmash({ normalizeNamesAs: 'camel' })).then(() => {
+  console.log('captured snapshots!');
+});
--- tests/test_oversmash.js
+++ tests/test_oversmash.js
@@ -1,96 +1,114 @@
-import assert from 'assert'
-import oversmash from '../lib'
-import { promisify } from 'util'
-import { readFile } from 'fs'
-import objectPath from 'object-path'
-import traverse from 'traverse'
-import debug from 'debug'
+import assert from 'assert';
+import oversmash from '../lib';
+import { promisify } from 'util';
+import { readFile } from 'fs';
+import objectPath from 'object-path';
+import traverse from 'traverse';
+import debug from 'debug';
 
-const log = debug('oversmash:snapshot')
-const readFileAsync = promisify(readFile)
+const log = debug('oversmash:snapshot');
+const readFileAsync = promisify(readFile);
 
-async function loadSnapshot (name) {
-  const data = await readFileAsync(`./tests/snapshots/${name}.json`)
-  return JSON.parse(data)
+async function loadSnapshot(name) {
+  const data = await readFileAsync(`./tests/snapshots/${name}.json`);
+  return JSON.parse(data);
 }
 
-function compareToSnapshot (snapshot, data) {
+function compareToSnapshot(snapshot, data) {
   traverse(data).forEach(function (node) {
-    if (Array.isArray(node) || (typeof node === 'object' && node !== null)) return
+    if (Array.isArray(node) || (typeof node === 'object' && node !== null))
+      return;
 
-    const path = this.path.join('.')
-    const compareTo = objectPath.get(snapshot, path)
-    const t = typeof node
-    const ct = typeof compareTo
+    const path = this.path.join('.');
+    const compareTo = objectPath.get(snapshot, path);
+    const t = typeof node;
+    const ct = typeof compareTo;
 
     if (t === 'undefined' || node === null) {
-      log(`bad-value(${path}): is ${t === null ? 'null' : 'undefined'}`)
+      log(`bad-value(${path}): is ${t === null ? 'null' : 'undefined'}`);
     }
 
     if (t !== ct) {
-      log(`type-diverged(${path}): ${t} !== ${ct} (new !== old)`)
+      log(`type-diverged(${path}): ${t} !== ${ct} (new !== old)`);
     }
 
     if (node !== compareTo) {
-      log(`diverged(${path}): ${node} !== ${compareTo} (new !== old)`)
+      log(`diverged(${path}): ${node} !== ${compareTo} (new !== old)`);
     }
-  })
+  });
 }
 
-async function testPlayerProfile (ow, snapshot) {
-  const p = await ow.player('adara#21451')
+async function testPlayerProfile(ow, snapshot) {
+  const p = await ow.player('adara#21451');
 
-  assert.strictEqual(p.name, 'adara#21451')
-  assert.strictEqual(p.nameEscaped, 'adara-21451')
-  assert.strictEqual(p.nameEscapedUrl, 'adara%2321451')
-  assert.strictEqual(p.accounts.length, 1)
+  assert.strictEqual(p.name, 'adara#21451');
+  assert.strictEqual(p.nameEscaped, 'adara-21451');
+  assert.strictEqual(p.nameEscapedUrl, 'adara%2321451');
+  assert.strictEqual(p.accounts.length, 1);
 
-  compareToSnapshot(snapshot, p)
+  compareToSnapshot(snapshot, p);
 }
 
-async function testPlayerStats (ow, snapshot) {
-  const p = await ow.playerStats('adara#21451', 'pc')
+async function testPlayerStats(ow, snapshot) {
+  const p = await ow.playerStats('adara#21451', 'pc');
 
-  assert.strictEqual(p.name, 'adara#21451')
-  assert.strictEqual(p.nameEscaped, 'adara-21451')
-  assert.strictEqual(p.platform, 'pc')
+  assert.strictEqual(p.name, 'adara#21451');
+  assert.strictEqual(p.nameEscaped, 'adara-21451');
+  assert.strictEqual(p.platform, 'pc');
 
-  assert(p.stats.endorsementLevel > 0, 'expected endorsement level to be above zero')
-  assert(p.stats.gamesWon > 2000, 'expected games won to be a number above the given value')
+  assert(
+    p.stats.endorsementLevel > 0,
+    'expected endorsement level to be above zero'
+  );
+  assert(
+    p.stats.gamesWon > 2000,
+    'expected games won to be a number above the given value'
+  );
 
-  const oneRoleMatched = ['tank', 'damage', 'support'].some(role => {
-    return p.stats.competitiveRank[role] > 0
-  })
+  const oneRoleMatched = ['tank', 'damage', 'support'].some((role) => {
+    return p.stats.competitiveRank[role] > 0;
+  });
 
-  assert(oneRoleMatched, 'no competitive rank found for any role - missing placements?')
+  assert(
+    oneRoleMatched,
+    'no competitive rank found for any role - missing placements?'
+  );
 
   // Make sure diacritics replacement is working as intended:
-  assert(p.stats.quickplay.lucio.combat.allDamageDone > 0, 'failed to match stat for lucio, diacritics replacement might be broken')
+  assert(
+    p.stats.quickplay.lucio.combat.allDamageDone > 0,
+    'failed to match stat for lucio, diacritics replacement might be broken'
+  );
 
-  compareToSnapshot(snapshot, p)
+  compareToSnapshot(snapshot, p);
 }
 
-function testOptions (ow) {
-  assert.strictEqual(ow.options.requestOptions.headers['User-Agent'], 'oversmash tests')
-  assert.strictEqual(ow.options.requestOptions.baseUrl, 'https://playoverwatch.com/en-us')
+function testOptions(ow) {
+  assert.strictEqual(
+    ow.options.requestOptions.headers['User-Agent'],
+    'oversmash tests'
+  );
+  assert.strictEqual(
+    ow.options.requestOptions.baseUrl,
+    'https://playoverwatch.com/en-us'
+  );
 }
 
-async function runTests () {
+async function runTests() {
   const ow = oversmash({
     normalizeNamesAs: 'camel',
     requestOptions: {
       headers: {
-        'User-Agent': 'oversmash tests'
-      }
-    }
-  })
+        'User-Agent': 'oversmash tests',
+      },
+    },
+  });
 
-  testOptions(ow)
-  await testPlayerProfile(ow, await loadSnapshot('profile'))
-  await testPlayerStats(ow, await loadSnapshot('stats'))
+  testOptions(ow);
+  await testPlayerProfile(ow, await loadSnapshot('profile'));
+  await testPlayerStats(ow, await loadSnapshot('stats'));
 }
 
-runTests()
-  .then(() => {
-    console.log('OK!')
-  })
+runTests().then(() => {
+  console.log('OK!');
+});
